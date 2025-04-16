f768046fc47deb565cd7801aabc65e3491ae8f2e
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2025-04-16 02:19:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ setInterval(() => {
   const show = now.getSeconds() % 2 === 0; // Show every even second
   flashImg.style.display = show ? 'block' : 'none'; // Show/hide based on condition
 }, 500);
+
+document.addEventListener("mousemove", function (event) {
+  console.log("X: " + event.clientX + " Y: " + event.clientY);
+});
