2458fde54f4d6f4c4de6a239ec688aa0b4f676bc
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2025-04-16 14:21:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const flashImg = document.getElementById('flashImg');
+/*const flashImg = document.getElementById('flashImg');
 setInterval(() => {
   const now = new Date();
   const show = now.getSeconds() % 2 === 0;
   flashImg.style.display = show ? 'block' : 'none';
 }, 500);
-
+*/
 document.addEventListener("mousemove", function (event) {
   console.log("X: " + event.clientX + " Y: " + event.clientY);
 });
