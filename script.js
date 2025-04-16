16217df302cc31e258d43c5b7c7d3218acb3131d
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2025-04-16 02:52:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ setInterval(() => {
 document.addEventListener("mousemove", function (event) {
   console.log("X: " + event.clientX + " Y: " + event.clientY);
 });
+
+class Pokemon {
+  constructor(name, type, item1, item2, evolution, rarity, personalName = "Enter", hunger = 5, pokedexNumber = 0) {
+    this.name = name;
+    this.type = type;
+    this.item1 = item1;
+    this.item2 = item2;
+    this.evolution = evolution; // 1, 2, or 3
+    this.rarity = rarity;
+    this.personalName = personalName;
+    this.hunger = hunger;
+    this.pokedexNumber = pokedexNumber;
+  }
+}
+
+const user = {
+  goalType: "Study", // Choose from: Study, Read, Clean, PayBill, MakeCall, Computer, Write, Math
+  pokeball: 0,
+  bonusPokeball: 5,
+  candy: 0,
+  location: "Taupe",
+  berryPoints: 0,
+  team: [] // array of Pokémon objects
+};
+
+const zubat = new Pokemon("zubat", "poison", "berry", "none", 1, 0, "Enter", 5, 41);
+user.team.push(zubat);
